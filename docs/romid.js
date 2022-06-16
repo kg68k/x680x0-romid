85873639052fbb5cdd932b92e9f4747d4316cc8a
kg68k <78926718+kg68k@users.noreply.github.com>
2022-06-17 05:23:43 +0900

    support embedded model name

--- docs/romid.js
+++ docs/romid.js
@@ -55,20 +55,39 @@ function updateRomInfo () {
 
   const reader = new FileReader();
   reader.onload = async (event) => {
-    const sha256 = await getSha256String(event.target.result);
-    printRomInfo(file.size, sha256);
+    const fileData = event.target.result;
+
+    const sha256 = await getSha256String(fileData);
+    const romInfo = findRomInfo(file.size, sha256);
+    if (romInfo) {
+      printRomInfo(file.size, sha256, romInfo, "");
+      return;
+    }
+
+    const name = extractEmbeddedName(file.size, fileData);
+    if (name) {
+      const mod_data = fillEmbeddedNameByFF(file.size, fileData);
+      const mod_sha256 = await getSha256String(mod_data);
+      const mod_romInfo = findRomInfo(file.size, mod_sha256);
+      if (mod_romInfo) {
+        printRomInfo(file.size, mod_sha256, mod_romInfo, name);
+        return;
+      }
+    }
+
+    setText("sha256", sha256);
+    setErrorMessage("一致するハッシュ値がありません。未知のデータです。");
   };
   reader.readAsArrayBuffer(file);
 }
 
-function printRomInfo(size, sha256) {
-  setText("sha256", sha256);
+function findRomInfo(size, sha256) {
+  return RomData.find(obj => obj.size === size && obj.sha256 === sha256);
+}
 
-  const romInfo = RomData.find(obj => obj.size === size && obj.sha256 === sha256);
-  if (!romInfo) {
-    setErrorMessage("一致するハッシュ値がありません。未知のデータです。");
-    return;
-  }
+function printRomInfo(size, sha256, romInfo, name) {
+  setText("sha256", sha256);
+  setText("embeddedname", name);
 
   for (const [k, v] of Object.entries(romInfo)) {
     if (k === "url") {
@@ -78,6 +97,36 @@ function printRomInfo(size, sha256) {
       setText(k, v);
     }
   }
+}
+
+function extractEmbeddedName(size, fileData) {
+  if (![128*1024, 256*1024, 1024*1024].includes(size)) {
+    return null;
+  }
+
+  const d = Array.from(new Uint8Array(fileData, size-32, 4));
+  const s = d.map(v => String.fromCharCode(v)).join("");
+  if (s != 'NAME') {
+    return null;
+  }
+
+  const n = Array.from(new Uint8Array(fileData, size-28));
+  const nul_pos = n.indexOf(0);
+  n.length = (nul_pos >= 0) ? nul_pos : n.length;
+
+  function toPrintable(c) {
+    if (0x20 <= c && c <= 0x7e) {
+      return String.fromCharCode(c);
+    }
+    return "\\x" + c.toString(16).padStart(2, '0');
+  }
+  return n.map(c => toPrintable(c)).join("");
+}
+
+function fillEmbeddedNameByFF(size, fileData) {
+  const data = fileData.slice(0);
+  new Uint8Array(data, size-32).fill(0xff);
+  return data;
 }
 
 window.addEventListener("DOMContentLoaded", () => {
